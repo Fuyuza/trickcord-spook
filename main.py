263c618d95d837be1549259008a9b9c4a58c4ed0
Fuser <109461244+Fuyuza@users.noreply.github.com>
2022-10-27 13:22:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def ping(ctx):
 
 @candy.command()
 async def help(ctx):
-    await Paginator.Simple(PreviewsButton=Button(style=discord.ButtonStyle.grey, emoji='<:__:1035096189957320744>', NextButton=Button(style=discord.ButtonStyle.grey, emoji='<:__:1035095602003980340>', PageCounterStyle=discord.ButtonStyle.blurple).start(ctx, pages=embeds)
+    await Paginator.Simple(PreviewsButton=Button(style=discord.ButtonStyle.grey, emoji='<:__:1035096189957320744>'), NextButton=Button(style=discord.ButtonStyle.grey, emoji='<:__:1035095602003980340>'), PageCounterStyle=discord.ButtonStyle.blurple).start(ctx, pages=embeds)
 
 async def main():
     async with candy:
